717441f26c3c61a3a3a8c09357be751f608b31aa
gabriel subutzki portes <gabriels.p@MacBook-Air-de-gabriel.local>
2025-08-04 00:11:25 -0300

    Criaçao do botao login e modificaçoes visuais da aplicaçao

--- FervoApp/Source/Login/LoginViewV2.swift
+++ FervoApp/Source/Login/LoginViewV2.swift
@@ -19,23 +19,32 @@ struct LoginViewV2: View {
             .edgesIgnoringSafeArea(.all)  //Cor de fundo
         
             VStack{
-                Text("Bem-vindo(a) \n ao Fervo")
+                Text("Bem-vindo(a) \n ao Fervo!")
                     .foregroundStyle(.white)
                     .font(.title)
                     .fontWeight(.semibold)
                     .multilineTextAlignment(.center)
-                    .padding(.bottom, 100)
-                VStack(){
+                    .padding(.bottom, 10)
+                
+                Text("Por favor digite seu Email e Senha para continuar:")
+                    .foregroundStyle(.gray.opacity(0.9))
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 40)
+                    .font(.system(size: 16))
+                          
+                VStack(spacing: 12){
                     Text("Email:")
                         .font(.title2)
                         .foregroundStyle(.white)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 20)
                         
-                    TextField("Email", text: $Email)
+                    TextField("Email:", text: $Email)
+                        .font(.system(size: 23))
                         .keyboardType(.emailAddress)
-                        .background(Color.white.opacity(0.2))
-                        .cornerRadius(20)
+                        .background(Color.white.opacity(0.4))
+                        .cornerRadius(10)
                         .padding()
                    
                     Text("Senha:")
@@ -45,15 +54,35 @@ struct LoginViewV2: View {
                         .padding(.leading, 20)
                         
                     
-                    SecureField("Password", text: $password)
-                        .background(Color.white.opacity(0.2))
-                        .cornerRadius(20)
+                    SecureField("Senha:", text: $password)
+                        .font(.system(size: 23))
+                        .background(Color.white.opacity(0.4))
+                        .foregroundStyle(.black)
+                        .cornerRadius(10)
                         .padding()
+                                                
                 }
-                .frame(width: 300, height: 300, alignment: .center)
-                .background(Color.white.opacity(0.1))
+                .frame(width: 320, height: 250, alignment: .center)
+                .background(Color.gray.opacity(0.2))
                 .cornerRadius(22)
                 
+                //Adicionar o "Esqueceu a senha"
+                
+                VStack{
+                    Button {
+                        // ButtonConfig
+                    } label: {
+                        Text("Entrar")
+                    }
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .background(Color.blue.opacity(0.8))
+                    .foregroundStyle(.white)
+                    .cornerRadius(20)
+                    .font(.title3)
+                    .bold()
+
+                }
+                .padding(.top, 30)
                 
             }
             
