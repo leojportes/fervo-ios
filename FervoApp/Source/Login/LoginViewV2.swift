b81fae0fab1a7b838642d1427a26d8a9f48fe2e6
gabriel subutzki portes <gabriels.p@MacBook-Air-de-gabriel.local>
2025-08-04 17:15:07 -0300

    Ajustes do texto interno dos fields

--- FervoApp/Source/Login/LoginViewV2.swift
+++ FervoApp/Source/Login/LoginViewV2.swift
@@ -27,12 +27,6 @@ struct LoginViewV2: View {
                     .multilineTextAlignment(.center)
                     .padding(.bottom, 40)
                 
-//                Text("Por favor, digite seu Email e Senha para continuar:")
-//                    .foregroundStyle(.gray.opacity(0.9))
-//                    .fontWeight(.light)
-//                    .multilineTextAlignment(.center)
-//                    .padding(.bottom, 40)
-//                    .font(.system(size: 16))
                 
                 VStack(){               //area de login
                     Text("Email:")
@@ -41,31 +35,28 @@ struct LoginViewV2: View {
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 10)
                     
-                    HStack(){
-                        TextField(text: $Email){
-                            Text("Digite aqui seu Email")
-                                .foregroundStyle(.gray)
-                                
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.white.opacity(0.2))
+                                .frame(height: 45)
                             
+                            TextField("", text: $Email, prompt:   Text("Digite aqui seu Email").foregroundStyle(.gray))
+                                .padding(.leading, 10)
+                                .font(.system(size: 20))
+                                .foregroundStyle(.white)
+                                .keyboardType(.emailAddress)
                         }
-                            .font(.system(size: 20))
-                            .foregroundStyle(.white)
-                            .frame(height: 45)
-                            .keyboardType(.emailAddress)
-                            .background(Color.white.opacity(0.2))
-                            .cornerRadius(10)
-                            .padding(.bottom, 13)
-                            .padding(.leading,10)
-                            
+                        .padding(.bottom, 13)
                         
                         Image(systemName: "person.circle.fill")
                             .foregroundStyle(.white)
-                            .font(.system(size:35))
+                            .font(.system(size: 35))
                             .padding(.bottom, 13)
                             .padding(.trailing, 6)
-                        
                     }
-                    
+                    .padding(.trailing, 10)
+                    .padding(.leading, 10)
                     
                     Text("Senha:")
                         .font(.title2)
@@ -75,104 +66,105 @@ struct LoginViewV2: View {
                     
                     
                     HStack {
-                        SecureField(text: $password){
-                            Text("Digite aqui sua senha")
-                                .foregroundStyle(.gray)
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.white.opacity(0.2))
+                                .frame(height: 45)
+                            
+                            SecureField("", text: $password, prompt:   Text("Digite aqui sua senha").foregroundStyle(.gray))
                                 .font(.system(size: 20))
+                                .foregroundStyle(.white)
+                                .padding(.leading,10)
+                            
+                            
                         }
-                            .font(.system(size: 25))
-                            .frame(height: 45)
-                            .background(Color.white.opacity(0.2))
-                            .foregroundStyle(.black.opacity(0.5))
-                            .cornerRadius(10)
-                            .padding(.leading,10)
-                        
+                        .padding(.bottom, 13)
                         
                         Image(systemName: "key.fill")
                             .foregroundStyle(.white)
                             .font(.system(size: 30))
-                            .padding(.trailing, 15)
-                            .padding(.leading, 5)
-                        
-                        
+                            .padding(.bottom, 13)
+                            .padding(.trailing, 12)
+                            .padding(.leading, 8)
                     }
-                    
+                    .padding(.trailing, 10)
+                    .padding(.leading, 10)
                 }
                 .frame(width: 380, height: 230, alignment: .center)
-                .background(Color.gray.opacity(0.1))
+                .background(Color.gray.opacity(0.2))
                 .cornerRadius(10)
-                
-                HStack {
-                    
-                    Toggle(isOn: $rememberMe) {
-                        Text("Lembre-se de mim")
+                    HStack {
+                        
+                        Toggle(isOn: $rememberMe) {
+                            Text("Lembre-se de mim")
+                        }
+                        .toggleStyle(CheckboxTogglestyle())
+                        
+                        
+                        Button{                 //Forguet password Button
+                            //Button Configuration
+                        }label: {
+                            Text("Esqueceu sua senha?")
+                                .underline()
+                                .foregroundStyle(.white)
+                        }
+                        .padding(.trailing, 20)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
                     }
-                    .toggleStyle(CheckboxTogglestyle())
+                    .padding(.top, 10)
                     
-                    
-                    Button{                 //Forguet password Button
-                        //Button Configuration
-                    }label: {
-                        Text("Esqueceu sua senha?")
-                            .underline()
-                            .foregroundStyle(.white)
-                    }
-                    .padding(.trailing, 20)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                }
-                .padding(.top, 10)
-                
-                VStack{             //Enter button
-                    Button {
-                        // ButtonConfig
-                    } label: {
-                        Text("Entrar")
-                    }
-                    .frame(width: 250, height: 70, alignment: .center)
-                    .background(Color.blue.opacity(0.8))
-                    .foregroundStyle(.white)
-                    .cornerRadius(20)
-                    .font(.title3)
-                    .bold()
-                    
-                }
-                .padding(.top, 30)
-                
-                HStack{
-                    Text("Ainda não tem uma conta?")
+                    VStack{             //Enter button
+                        Button {
+                            // ButtonConfig
+                        } label: {
+                            Text("Entrar")
+                        }
+                        .frame(width: 250, height: 70, alignment: .center)
+                        .background(Color.blue.opacity(0.8))
                         .foregroundStyle(.white)
-                    Button {
-                        // ButtonConfig
-                    } label: {
-                        Text("Cadastrar-se")
-                            .underline()
+                        .cornerRadius(20)
+                        .font(.title3)
+                        .bold()
+                        
                     }
-
+                    .padding(.top, 30)
+                    
+                    HStack{
+                        Text("Ainda não tem uma conta?")
+                            .foregroundStyle(.white)
+                        Button {
+                            // ButtonConfig
+                        } label: {
+                            Text("Cadastrar-se")
+                                .underline()
+                        }
+                        
+                    }
+                    .padding()
                 }
-                .padding()
-            }
-            
-        }
-    }
-    
-struct CheckboxTogglestyle: ToggleStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        Button(action: {
-            configuration.isOn.toggle()
-        }) {
-            HStack {
-                Image(systemName: configuration.isOn ? "checkmark.square.fill" : "square")
-                    .foregroundColor(configuration.isOn ? .white : .gray)
-                    .padding(.leading, 14)
-                configuration.label
                 
             }
         }
-    }
-    }
     
-}
+        struct CheckboxTogglestyle: ToggleStyle {
+            func makeBody(configuration: Configuration) -> some View {
+                Button(action: {
+                    configuration.isOn.toggle()
+                }) {
+                    HStack {
+                        Image(systemName: configuration.isOn ? "checkmark.square.fill" : "square")
+                            .foregroundColor(configuration.isOn ? .white : .gray)
+                            .padding(.leading, 14)
+                        configuration.label
+                        
+                    }
+                }
+            }
+        }
+        
+    }
 
-#Preview{
-    LoginViewV2()
-}
+    #Preview{
+        LoginViewV2()
+    }
+
